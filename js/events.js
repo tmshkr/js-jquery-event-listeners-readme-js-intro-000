073eacbd861572239ef515dc34bd4db3249f426e
Tim Shaker <timshaker@gmail.com>
2018-08-23 03:19:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,7 +16,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $('form').on('submit', function(){
+  $('form').on("submit", function(){
     alert('Your form is going to be submitted now.')
   })
 }
