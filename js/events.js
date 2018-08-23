bc664e28d5fedcd28e340c707b7f9919e1faf6bf
Tim Shaker <timshaker@gmail.com>
2018-08-23 03:07:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,7 +5,7 @@ function getIt(){
 }
 
 function frameIt(){
-  document.querySelector('img').onload = this.class
+  document.querySelector('img').onload = this.className()
 }
 
 $(document).ready(function(){
